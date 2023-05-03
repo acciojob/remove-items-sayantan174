5fc283db38189f781ed923f90ddd4a6ca421afa4
Sayantan Mandal <sayantanmandal174@gmail.com>
2023-05-03 19:34:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
 //your JS code here. If required.
 function removeColor() {
   var select = document.getElementById("colorSelect");
-  var selectedColor = select.options[select.selectedIndex].value;
   
   // remove the selected option from the select element
   select.remove(select.selectedIndex);
-  
-  console.log("Removed color: " + selectedColor);
 }
 var button = document.getElementById("Select and Remove");
-button.addEventListener("click",removeColor());
 
 
